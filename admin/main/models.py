4d793be5a351796bb80c73ed6133b3caf159dc37
pure_water <103035453+EgorRedin@users.noreply.github.com>
2024-08-01 00:51:30 +0300

    dmksad

--- admin/main/models.py
+++ admin/main/models.py
@@ -3,8 +3,8 @@ from django.utils.timezone import now
 
 
 class UserRole(models.TextChoices):
-    ADMIN = 'ADMIN', 'Admin'
-    USER = 'USER', 'User'
+    user = 'USER', 'User'
+    admin = 'ADMIN', 'Admin'
 
 
 class Keyword(models.Model):
@@ -24,7 +24,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)  # Adjust max_length as needed
     key_word = models.ForeignKey(Keyword, on_delete=models.CASCADE, related_name='users')
-    role = models.CharField(max_length=5, choices=UserRole.choices, default=UserRole.USER)
+    role = models.CharField(max_length=5, choices=UserRole.choices, default=UserRole.user)
     phone_number = models.CharField(max_length=15, null=True, blank=True)  # Adjust max_length as needed
     chat_id = models.CharField(max_length=255, null=True, blank=True)  # Adjust max_length as needed
     date = models.DateTimeField(default=now)
